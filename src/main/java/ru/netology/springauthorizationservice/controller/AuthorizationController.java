559829d9adb733ed693b1241634b818aea4db4d1
Artos test <korshenkov.vlad@yandex.ru>
2025-06-24 06:00:57 +0300

    Nginx

--- src/main/java/ru/netology/springauthorizationservice/controller/AuthorizationController.java
+++ src/main/java/ru/netology/springauthorizationservice/controller/AuthorizationController.java
@@ -1,7 +1,5 @@
 package ru.netology.springauthorizationservice.controller;
 
-
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
